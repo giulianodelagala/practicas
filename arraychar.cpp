ede298b8e6b99b058368c5023ad92415edede581
ucsp <alexander.pinto@ucsp.edu.pe>
2015-09-10 09:53:34 -0500

    practica array string

---
+++ arraychar.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+ using namespace std;
+
+int main() 
+{
+	char hola[] = {'H', 'e', 'l', 'l', 'o', ',', ' ',
+		'w', 'o', 'r', 'l', 'd', '!', '\0' };
+	cout << hola << endl;
+	return 0;
+}
