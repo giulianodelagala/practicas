18664bfe518b499a7daf01b11b919b64a1aedd72
giulianodelagala <alexander.pinto@ucsp.edu.pe>
2015-09-28 16:57:43 -0500

    Create problema7.cpp

---
+++ problemas/problema7.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+
+bool espalindrome (string palabra, int i, int tamanio, int ultimo)
+{
+	//cout << palabra[0]; 
+
+
+	if (tamanio <= 1)
+	{return true;}
+	else if (palabra[i] == palabra[ultimo])
+	{
+		//cout << "i" << i << "tam=" << tamanio << "ul" << ultimo << endl;
+		return espalindrome (palabra, i+1, tamanio-2, ultimo-1);
+	}
+	else
+	{
+	return false;}
+}
+	
+
+
+
+
+int main ()
+{
+	bool palindrome = false;
+	long num;
+	string palabra; 
+	for (int i = 999; i > 10; i--)
+	{
+		for (int j = 999; j > 10; j--)
+		{
+			num = i * j;
+			//cout << num;
+			ostringstream convert;   // stream used for the conversion
+			convert << num;      // insert the textual representation of 'Number' in the characters in the stream
+			palabra = convert.str();
+			//cout << palabra;
+			int tamanio = palabra.length();
+			//cout << tamanio;
+			
+			palindrome = espalindrome(palabra, 0, tamanio, tamanio-1);
+			if (palindrome)
+			{
+				cout << i << " x " << j << "= ";
+				cout << num << " es palindrome";
+				break;
+			}
+			
+		}
+		if (palindrome)
+		break;
+	}
+	
+	
+	
+	//cout << palabra[0];
+	return 0;
+}
