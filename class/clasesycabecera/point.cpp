d1b67fd76396958b30d29dcc16aa91203f840ba5
Alexander Pinto <alexander.pinto@ucsp.edu.pe>
2015-10-28 10:08:10 -0500

    point.cpp

---
+++ class/clasesycabecera/point.cpp
@@ -0,0 +1,21 @@
+#include "point.h"
+
+	double Point::getx()
+	{
+		return _x;
+	}
+			
+	double Point::gety()
+	{
+		return _y;
+	}
+			
+	void Point::setx(double x)
+	{
+		_x = x;
+	}
+	
+	void Point::sety (double y)
+	{
+		_y = y;
+	}
