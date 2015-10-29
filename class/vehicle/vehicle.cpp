c21fb3e1d199f7dd253e6d455e093061447fe1e3
Alexander Pinto <alexander.pinto@ucsp.edu.pe>
2015-10-28 20:04:33 -0500

    herencia

--- class/vehicle/vehicle.cpp
+++ class/vehicle/vehicle.cpp
@@ -1,1 +1,59 @@
 class Vehicle
+{
+	protected:
+		string license;
+		int year;
+	
+	public:
+		Vehicle (const string &myLicense, const int myYear)
+		: license (myLicense), year (myYear)
+		{}
+		
+		const string getDesc() const
+		{
+			return license + "from" + stringify (year);
+		}
+		const string &getLicense () const
+		{
+			return license;
+		}
+		const int getYear()
+		const
+		{
+			return year;
+		}
+};
+	
+class Car : public Vehicle
+{
+	string style;
+	
+	public:
+		Car (const string &myLicense, const int myYear,
+		 const string &myStyle) : Vehicle(myLicense, myYear), 
+		 style(myStyle)
+		 {}
+		const string &getStyle()
+		{
+			return style;
+		}
+		
+}; 
+
+class Truck : public Vehicle
+{
+	string style;
+	
+	public:
+		Car (const string &myLicense, const int myYear,
+		 const string &myStyle) : Vehicle(myLicense, myYear), 
+		 style(myStyle)
+		 {}
+		const string &getStyle()
+		{
+			return style;
+		}
+		
+}; 	
+		
+		
